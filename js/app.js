f1af019377e6b77a50405c28548c345c9e072fe6
STEPHEN CHU <stephen.chu530@gmail.com>
2019-04-02 15:14:34 -0700

    feat: added render hourly totals

--- js/app.js
+++ js/app.js
@@ -23,7 +23,20 @@ StoreLocation.prototype.calcCookiesPurchasedPerDay = function () {
 };
 
 // Method to render table row for store
-StoreLocation.prototype.renderRow = function () {
+StoreLocation.prototype.renderRow = function (table) {
+  let row = document.createElement('tr');
+  let data = document.createElement('th');
+  data.textContent = this.storeName;
+  row.appendChild(data);
+  this.cookiesPurchasedEachHour.forEach(function(count) {
+    data = document.createElement('td');
+    data.textContent = count;
+    row.appendChild(data);
+  });
+  data = document.createElement('td');
+  data.textContent = this.totalCookiesPerDay;
+  row.appendChild(data);
+  table.appendChild(row);
 };
 
 // Helper function to get random number of customers
@@ -49,13 +62,18 @@ function renderDailySalesTable() {
   let sectionEl = document.getElementById('dailySales');
   let table = document.createElement('table');
   renderHeader(table);
+  allStores.forEach(function(store) {
+    store.renderRow(table);
+  });
+  renderHourlyTotals(table);
   sectionEl.appendChild(table);
 }
 
 // Helper function to render table header
 function renderHeader(table) {
-  let row = document.createElement('row');
-  var header = document.createElement('th');
+  let row = document.createElement('tr');
+  let header = document.createElement('td');
+  row.appendChild(header);
   storeHours.forEach(function(item) {
     header = document.createElement('th');
     header.textContent = item;
@@ -64,6 +82,29 @@ function renderHeader(table) {
   header = document.createElement('th');
   header.textContent = 'Daily Location Total';
   row.appendChild(header);
+  table.appendChild(row);
+}
+
+// Function to calculate and render hourly total row
+function renderHourlyTotals(table) {
+  let row = document.createElement('tr');
+  let data = document.createElement('th');
+  data.textContent = 'Totals';
+  row.appendChild(data);
+  let total = 0;
+  for (let i = 0; i < storeHours.length; i++) {
+    let subTotal = 0;
+    allStores.forEach(function(store) {
+      subTotal += store.cookiesPurchasedEachHour[i];
+    });
+    data = document.createElement('td');
+    data.textContent = subTotal;
+    row.appendChild(data);
+    total += subTotal;
+  }
+  data = document.createElement('td');
+  data.textContent = total;
+  row.appendChild(data);
   table.appendChild(row);
 }
 
@@ -76,5 +117,5 @@ let storeSpecs = [
   ['Alki', 2, 16, 4.6],
 ];
 let storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-renderDailySalesTable();
 let allStores = makeStores();
+renderDailySalesTable();
